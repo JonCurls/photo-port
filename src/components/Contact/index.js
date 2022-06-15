69e5af3b4d2537435ac84ac756e4b45fbb28e1fb
MrCurls <JonCurls@gmail.com>
2022-06-14 22:54:38 -0500

    form submit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,54 @@
-import react from "react";
+import React, { useState } from "react";
 
-function ContractForm() {
+function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
   //JSX
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            defaultValue={name}
+            onChange={handleChange}
+            name="name"
+          />
         </div>
         <div>
           <label htmlFor="email">Email Address:</label>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            defaultValue={email}
+          />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea
+            name="message"
+            rows="5"
+            onChange={handleChange}
+            defaultValue={message}
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
@@ -22,4 +56,4 @@ function ContractForm() {
   );
 }
 
-export default ContractForm;
+export default ContactForm;
